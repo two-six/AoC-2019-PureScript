33d1bc7739da77352ab4b728510218d08422cf4c
two-six <twopsix@gmail.com>
2022-04-11 00:56:06 +0200

    Removed a lot fromMaybe's

--- 2/src/Main.purs
+++ 2/src/Main.purs
@@ -22,10 +22,17 @@ readNumbers :: String -> Array Int
 readNumbers = map (maybe 0 identity <<< fromString) <<< split (Pattern ",")
 
 opcode :: Array Int -> Int -> (Int -> Int -> Int) -> Array Int
-opcode a n f = do
-  fromMaybe []
-    $ modifyAt (fromMaybe 0 $ index a (n+3))
-    (\_ -> f (fromMaybe 0 $ (index a (fromMaybe 0 $ index a (n+1)))) (fromMaybe 0 $ (index a (fromMaybe 0 $ index a (n+2))))) a
+opcode arr n f =
+  fromMaybe [] opcodeMaybe
+  where
+    opcodeMaybe :: Maybe (Array Int)
+    opcodeMaybe = do
+      a <- index arr (n+1)
+      b <- index arr (n+2)
+      c <- index arr (n+3)
+      x <- index arr a
+      y <- index arr b
+      modifyAt c (\_ -> f x y) arr
 
 cmt :: Array Int -> Int -> Array Int
 cmt arr' n =
@@ -36,14 +43,26 @@ cmt arr' n =
 
 computeS :: Array Int -> Array Int
 computeS arr =
-  cmt (fromMaybe [] (modifyAt 2 (\_ -> 2) (fromMaybe [] (modifyAt 1 (\_ -> 12) arr)))) 0
+  fromMaybe [] computeSMaybe
+  where
+    computeSMaybe :: Maybe (Array Int)
+    computeSMaybe = do
+      arr1 <- modifyAt 1 (\_ -> 12) arr
+      arr2 <- modifyAt 2 (\_ -> 2) arr1
+      pure $ cmt arr2 0
 
 silver :: String -> Int
 silver str = fromMaybe 0 (head $ computeS $ readNumbers str)
 
 modifiedArray :: Int -> Int -> Array Int -> Array Int
 modifiedArray x y arr =
-  fromMaybe [] (modifyAt 2 (\_ -> y) (fromMaybe [] (modifyAt 1 (\_ -> x) arr)))
+  fromMaybe [] modifiedArrayMaybe
+  where
+    modifiedArrayMaybe :: Maybe (Array Int)
+    modifiedArrayMaybe = do
+      arr1 <- modifyAt 1 (\_ -> x) arr
+      arr2 <- modifyAt 2 (\_ -> y) arr1
+      pure arr2
 
 computeG :: Array Int -> Array Int
 computeG arr = do
